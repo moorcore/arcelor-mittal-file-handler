563b8301eecaecbc8a11ffa8a9bae944000cbd8d
Maxim Boykin <deuswn@gmail.com>
2022-06-28 19:28:50 +0200

    Initial commit

---
+++ ArcelorFileHandler/Program.cs
@@ -0,0 +1,23 @@
+﻿using ArcelorFileHandler.Services;
+using System;
+
+namespace ArcelorFileHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // FileManager.DirSearch("D:\\Coding\\Arcelor Mittal\\Сентябрь");
+
+            string xlFilePath = 
+                "D:\\Coding\\Arcelor Mittal\\Приложения\\Август\\1_Приложение 1 с 01 по 05 августа 2021 г";
+
+            ExcelApi excelApi = new ExcelApi(xlFilePath);
+
+            string cellValue = excelApi.GetCellData("Sheet1", 1, 6);
+            Console.WriteLine("Cell Value using Column Number: " + cellValue);
+
+            Console.Read();
+        }
+    }
+}
---
+++ ArcelorFileHandler/Services/FileManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcelorFileHandler.Services
+{
+    public static class FileManager
+    {
+        public static void DirSearch(string path)
+        {
+            try
+            {
+                foreach (string dir in Directory.GetDirectories(path))
+                {
+                    foreach (string file in Directory.GetFiles(dir))
+                    {
+                        Console.WriteLine(file);
+                    }
+                    DirSearch(dir);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
