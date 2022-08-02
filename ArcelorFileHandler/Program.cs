21ef8ad0416343297792c260db04d506f9534cbc
Maxim Boykin <deuswn@gmail.com>
2022-08-02 16:51:34 +0200

    Upd: minor logic fixes

--- ArcelorFileHandler/Program.cs
+++ ArcelorFileHandler/Program.cs
@@ -1,8 +1,4 @@
 ﻿using ArcelorFileHandler.Services;
-using IronXL;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace ArcelorFileHandler
 {
@@ -12,27 +8,15 @@ namespace ArcelorFileHandler
         static void Main(string[] args)
         {
             FileManager fileManager = new FileManager();
-            fileManager.DirSearch("D:\\Coding\\Arcelor Mittal\\Сентябрь");
+            // fileManager.DirSearch("D:\\Coding\\Arcelor Mittal\\Сентябрь");
 
             // var invoiceNumbersArray = fileManager.invoiceNumbersList.ToArray();
 
             string filePath =
                 "D:\\Coding\\Arcelor Mittal\\Приложения\\Август\\1_Приложение 1 с 01 по 05 августа 2021 г.xlsx";
 
-            WorkBook wb = WorkBook.Load(filePath);
-            WorkSheet ws = wb.GetWorkSheet("П 42_2021 к Акту №549");
-
-            Console.Read();
-
-            var array = ws["F11:F34"].ToArray();
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                var cellValue = array[i];
-                Console.WriteLine(cellValue.ToString().Substring(1));
-            }
-
-            Console.Read();
+            ExcelApi excelApi = new ExcelApi();
+            excelApi.GetInvoiceNumbers(filePath);
         }
     }
 }
--- ArcelorFileHandler/Services/ExcelApi.cs
+++ ArcelorFileHandler/Services/ExcelApi.cs
@@ -1,9 +1,43 @@
-﻿using System;
+﻿using IronXL;
+using System;
+using System.Linq;
 
 namespace ArcelorFileHandler.Services
 {
     public class ExcelApi
     {
-        
+        public void GetInvoiceNumbers(string path)
+        {
+            int invoiceNumbersCount = 0;
+
+            WorkBook workBook = WorkBook.Load(path);
+
+            foreach (var worsheet in workBook.WorkSheets)
+            {
+                var array = worsheet["F11:F34"].ToArray();
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    var cellValue = array[i];
+                    Console.WriteLine(cellValue.ToString().Substring(1));
+                    invoiceNumbersCount++;
+                }
+            }
+
+            /*            WorkSheet ws = workBook.GetWorkSheet("П 42_2021 к Акту №549");
+
+                        var array = ws["F11:F34"].ToArray();
+
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            var cellValue = array[i];
+                            Console.WriteLine(cellValue.ToString().Substring(1));
+                            invoiceNumbersCount++;
+                        }*/
+
+            Console.WriteLine(invoiceNumbersCount); // was 24
+
+            Console.Read();
+        }
     }
 }
