72e9f939813807dd063b7664edfc8d5e5e5f656d
Maxim Boykin <deuswn@gmail.com>
2022-08-05 18:06:34 +0200

    Upd: minor logic update

--- ArcelorFileHandler/Services/ExcelApi.cs
+++ ArcelorFileHandler/Services/ExcelApi.cs
@@ -8,10 +8,43 @@ namespace ArcelorFileHandler.Services
     {
         public string _startingCellLiteral { get; set; } = "F";
         public string _endingCellLiteral { get; set; } = "F";
-        public string _startingCellNumber { get; set; }
-        public string _endingCellNumber { get; set; }
+        public int _startingCellNumber { get; set; } = 11;
+        public int _endingCellNumber { get; set; } = 1000;
 
         public void GetInvoiceNumbers(string path, string startingCell = "F11", string endingCell = "F1000")
+        {
+            startingCell = $"{_startingCellLiteral}{_startingCellNumber}";
+            endingCell = $"{_endingCellLiteral}{_endingCellNumber}";
+
+            string currentCellLiteral = _startingCellLiteral;
+            int currentCellNumber = _startingCellNumber;
+
+            WorkBook workBook = WorkBook.Load(path);
+
+            foreach (var worksheet in workBook.WorkSheets)
+            {
+                var cell = worksheet[$"{currentCellLiteral}{currentCellNumber}"];
+
+                while (!cell.IsEmpty)
+                {
+                    if (cell.Value.ToString().Length >= 7)
+                    {
+                        var cellValue = cell.Value.ToString();
+                        Console.WriteLine(cellValue.Substring(1));
+                    }
+
+                    currentCellNumber++;
+
+                    cell = worksheet[$"{currentCellLiteral}{currentCellNumber}"];
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.Read();
+        }
+
+/*        public void GetInvoiceNumbers(string path, string startingCell = "F11", string endingCell = "F1000")
         {
             startingCell = $"{_startingCellLiteral}{_startingCellNumber}";
             endingCell = $"{_endingCellLiteral}{_endingCellNumber}";
@@ -41,6 +74,6 @@ namespace ArcelorFileHandler.Services
             Console.WriteLine();
 
             Console.Read();
-        }
+        }*/
     }
 }
