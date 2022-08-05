84be985da96808029d338f2a5830ac427c6791ee
Maxim Boykin <deuswn@gmail.com>
2022-08-05 18:30:40 +0200

    Upd: minor fixes

--- ArcelorFileHandler/Services/ExcelApi.cs
+++ ArcelorFileHandler/Services/ExcelApi.cs
@@ -7,23 +7,25 @@ namespace ArcelorFileHandler.Services
     public class ExcelApi
     {
         public string _startingCellLiteral { get; set; } = "F";
-        public string _endingCellLiteral { get; set; } = "F";
         public int _startingCellNumber { get; set; } = 11;
-        public int _endingCellNumber { get; set; } = 1000;
 
-        public void GetInvoiceNumbers(string path, string startingCell = "F11", string endingCell = "F1000")
+        public void GetInvoiceNumbers(string path)
         {
-            startingCell = $"{_startingCellLiteral}{_startingCellNumber}";
-            endingCell = $"{_endingCellLiteral}{_endingCellNumber}";
-
             string currentCellLiteral = _startingCellLiteral;
             int currentCellNumber = _startingCellNumber;
+
+            int count = 0;
 
             WorkBook workBook = WorkBook.Load(path);
 
             foreach (var worksheet in workBook.WorkSheets)
             {
+                // TODO: reset the cell number for each worksheet
                 var cell = worksheet[$"{currentCellLiteral}{currentCellNumber}"];
+
+                Console.WriteLine();
+                Console.WriteLine(worksheet.Name);
+                Console.WriteLine();
 
                 while (!cell.IsEmpty)
                 {
@@ -31,6 +33,7 @@ namespace ArcelorFileHandler.Services
                     {
                         var cellValue = cell.Value.ToString();
                         Console.WriteLine(cellValue.Substring(1));
+                        count++;
                     }
 
                     currentCellNumber++;
@@ -41,39 +44,9 @@ namespace ArcelorFileHandler.Services
 
             Console.WriteLine();
 
+            Console.WriteLine(count);
+
             Console.Read();
         }
-
-/*        public void GetInvoiceNumbers(string path, string startingCell = "F11", string endingCell = "F1000")
-        {
-            startingCell = $"{_startingCellLiteral}{_startingCellNumber}";
-            endingCell = $"{_endingCellLiteral}{_endingCellNumber}";
-
-            WorkBook workBook = WorkBook.Load(path);
-
-            foreach (var worksheet in workBook.WorkSheets)
-            {
-                var array = worksheet[$"{startingCell}:{endingCell}"].ToArray();
-                Console.WriteLine(worksheet.RowCount);
-
-                for (int i = 0; i < array.Length; i++)
-                {
-                    if (array[i].IsEmpty)
-                    {
-                        continue;
-                    }
-
-                    if (array[i].Text.Length >= 7)
-                    {
-                        var cellValue = array[i];
-                        Console.WriteLine(cellValue.ToString().Substring(1));
-                    }
-                }
-            }
-
-            Console.WriteLine();
-
-            Console.Read();
-        }*/
     }
 }
